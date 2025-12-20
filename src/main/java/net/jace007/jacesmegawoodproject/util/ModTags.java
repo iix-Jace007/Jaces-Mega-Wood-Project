8c283bfff75fae8917899239b81b2bdfeef36276
iix-Jace007 <juliaarinsberg07@gmail.com>
2025-12-19 22:27:25 -0500

    Added Mod Tags Class

---
+++ src/main/java/net/jace007/jacesmegawoodproject/util/ModTags.java
@@ -0,0 +1,24 @@
+package net.jace007.jacesmegawoodproject.util;
+
+import net.jace007.jacesmegawoodproject.JacesMegaWoodProject;
+import net.minecraft.resources.Identifier;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+
+public class ModTags {
+    public static class Blocks {
+
+        private static TagKey<Block> createTag(String name) {
+            return BlockTags.create(Identifier.fromNamespaceAndPath(JacesMegaWoodProject.MOD_ID, name));
+        }
+    }
+    public static class Items {
+
+        private static TagKey<Item> createTag(String name) {
+            return ItemTags.create(Identifier.fromNamespaceAndPath(JacesMegaWoodProject.MOD_ID, name));
+        }
+    }
+}
