36d38517992ffcc56c7688aa50e3561a83810aeb
iix-Jace007 <juliaarinsberg07@gmail.com>
2025-12-23 22:57:33 -0500

    updated some particle stuff still needs work

--- src/main/java/net/jace007/jacesmegawoodproject/particle/JMWPParticles.java
+++ src/main/java/net/jace007/jacesmegawoodproject/particle/JMWPParticles.java
@@ -3,6 +3,7 @@ package net.jace007.jacesmegawoodproject.particle;
 import net.jace007.jacesmegawoodproject.JacesMegaWoodProject;
 import net.minecraft.client.particle.FallingLeavesParticle;
 import net.minecraft.core.particles.ParticleType;
+import net.minecraft.core.particles.SimpleParticleType;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -13,8 +14,8 @@ public class JMWPParticles {
         public static final DeferredRegister<ParticleType<?>> PARTICLE_TYPES =
                 DeferredRegister.create(BuiltInRegistries.PARTICLE_TYPE, JacesMegaWoodProject.MOD_ID);
 
-        public static final Supplier<FallingLeavesParticle> AGRABAH_CEDER_PARTICALES =
-                PARTICLE_TYPES.register("agrabah_ceder_particles", () -> new FallingLeavesParticle(true));
+        public static final Supplier<SimpleParticleType> AGRABAH_CEDER_PARTICALES =
+                PARTICLE_TYPES.register("agrabah_ceder_particles", () -> new SimpleParticleType(true));
 
         public static void register(IEventBus eventBus) {
                 PARTICLE_TYPES.register(eventBus);
